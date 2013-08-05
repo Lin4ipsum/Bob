ea893f7d760a5a42f31d17840bd0dc656551d5ad
Linda <lin.m.gonzalez@gmail.com>
2013-08-05 12:37:53 -0400

    make sure that #hey has 2 spaces in conditional

--- bob.rb
+++ bob.rb
@@ -2,13 +2,13 @@ class Bob
 
   def hey string
     if empty(string)
-     'Fine. Be that way!'  
+      'Fine. Be that way!'  
     elsif shouting(string)
-     'Woah, chill out!' 
+      'Woah, chill out!' 
     elsif question(string)
-     'Sure.'  
+      'Sure.'  
     else
-     'Whatever.'
+      'Whatever.'
     end
   end
 
