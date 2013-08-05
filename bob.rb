9e1715e0b8b1b6872fc4c546a9dc85bb81f22645
Linda <lin.m.gonzalez@gmail.com>
2013-08-05 11:59:27 -0400

    update method names to make them more semantic

--- bob.rb
+++ bob.rb
@@ -1,23 +1,23 @@
 class Bob
 
   def hey string
-    return 'Fine. Be that way!' if check_for_nil_and_empty(string)
-    return 'Woah, chill out!' if check_for_upcase(string)
-    return 'Sure.' if check_for_ends_in_question_mark(string)
+    return 'Fine. Be that way!' if empty(string)
+    return 'Woah, chill out!' if shouting(string)
+    return 'Sure.' if question(string)
     return 'Whatever.'
   end
 
   private
 
-  def check_for_nil_and_empty string
+  def empty string
     string.to_s.strip.empty?
   end
 
-  def check_for_upcase string
+  def shouting string
     string == string.upcase
   end
 
-  def check_for_ends_in_question_mark string
+  def question string
     string.end_with?('?')
   end
 
